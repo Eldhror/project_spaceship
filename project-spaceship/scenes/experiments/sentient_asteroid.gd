4d4ef028a96a5321b2d12fecb3f3973e82db97e4
Flodurs <76902177+Flodurs@users.noreply.github.com>
2026-07-04 17:52:49 +0200

    augen

--- project-spaceship/scenes/experiments/sentient_asteroid.gd
+++ project-spaceship/scenes/experiments/sentient_asteroid.gd
@@ -7,7 +7,7 @@ var rng = RandomNumberGenerator.new()
 func _enderman() -> void:
 	var jumprange = 5
 	var dX = rng.randf_range(-jumprange, jumprange)
-	var dY = rng.randf_range(-jumprange, jumprange)
+	var dY = rng.randf_range(0, jumprange)
 	var dZ = rng.randf_range(-jumprange, jumprange)
 	$".".translate(Vector3(dX, dY, dZ)) 
 	$AudioStreamPlayer3D.play()
@@ -15,7 +15,6 @@ func _enderman() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
