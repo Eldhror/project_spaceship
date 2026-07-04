0ac10eef1b8c05b6beeb48eb898830b4f584a9fe
Paul Schulz <eldhror@fedora.fritz.box>
2026-07-04 17:46:05 +0200

    improved player controller and modified my test scene

--- project-spaceship/scenes/player/player.gd
+++ project-spaceship/scenes/player/player.gd
@@ -1,23 +1,50 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED: float = 20
+const JUMP_VELOCITY: float = 4.5
 
+var mouse_sensitivity: float = 0.01
+
+var twist_input: float = 0
+var pitch_input: float = 0
+var cam_pitch_max: int = 60
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
+	var direction: Vector3
+	var input: Vector3 = Vector3.ZERO
+	
+		#check wether mouse escaped
+	if Input.is_action_just_pressed("Esc"):
+		if Input.mouse_mode == 2:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	$TwistInput.rotate_y(twist_input)
+	$TwistInput/PitchInput.rotate_z(pitch_input)
+	$TwistInput/PitchInput.rotation.z = clamp($TwistInput/PitchInput.rotation.z, deg_to_rad(-cam_pitch_max), deg_to_rad(cam_pitch_max))
+	
+	twist_input = 0
+	pitch_input = 0
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	input.x = Input.get_axis("backward", "forward")
+	input.z = Input.get_axis("left", "right")
+
+	direction = ($TwistInput.global_basis * input).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +53,9 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
