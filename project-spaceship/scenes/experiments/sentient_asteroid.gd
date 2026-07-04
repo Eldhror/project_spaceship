e0635ca222df0450dfb2f3778e9708e9c2b4095b
Flodurs <76902177+Flodurs@users.noreply.github.com>
2026-07-04 16:15:31 +0200

    blink blink

---
+++ project-spaceship/scenes/experiments/sentient_asteroid.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+var timer = 0.0
+var state = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	timer += delta
+	if timer > 2000:
+		timer = 0
+		state+=1
+		
+	if state == 1:
+		var material = $MeshInstance3D.get_surface_material(0)
+		material.albedo_color = Color(1, 0, 0)
+		$MeshInstance.set_surface_material(0, material)
+	
+	if state == 2:
+		var material = $MeshInstance3D.get_surface_material(0)
+		material.albedo_color = Color(0, 1, 0)
+		$MeshInstance.set_surface_material(0, material)
+	
+	if state == 3:
+		state = 1
