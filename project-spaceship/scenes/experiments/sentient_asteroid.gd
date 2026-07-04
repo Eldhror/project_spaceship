16eec2b33e8cd31108a7babedb22c901335b6ea0
Flodurs <76902177+Flodurs@users.noreply.github.com>
2026-07-04 16:27:35 +0200

    blink blink works

--- project-spaceship/scenes/experiments/sentient_asteroid.gd
+++ project-spaceship/scenes/experiments/sentient_asteroid.gd
@@ -11,19 +11,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	timer += delta
-	if timer > 2000:
+	if timer > 0.2:
 		timer = 0
 		state+=1
 		
 	if state == 1:
-		var material = $MeshInstance3D.get_surface_material(0)
+		var material = $MeshInstance3D.get_surface_override_material(0)
 		material.albedo_color = Color(1, 0, 0)
-		$MeshInstance.set_surface_material(0, material)
+		$MeshInstance3D.set_surface_override_material(0, material)
 	
 	if state == 2:
-		var material = $MeshInstance3D.get_surface_material(0)
+		var material = $MeshInstance3D.get_surface_override_material(0)
 		material.albedo_color = Color(0, 1, 0)
-		$MeshInstance.set_surface_material(0, material)
+		$MeshInstance3D.set_surface_override_material(0, material)
 	
 	if state == 3:
 		state = 1
